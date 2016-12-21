8647d1b23604d210ca3300bcfc24c85ee6ec61c4
Daniel Morrison <daniel.morrison@bbc.co.uk>
2016-12-21 01:09:12 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodestash-shipper
 Shipper for Nodestash
+
+Work in progress, tests and docs to come as of 21st December 2016
+Do not use
---
+++ example/index.js
@@ -0,0 +1,3 @@
+const Shipper = require('../index.js');
+const config = require('./configuration.json');
+const cargo = new Shipper(config);
